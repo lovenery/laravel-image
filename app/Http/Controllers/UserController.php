01cf789dd95d45902cc870a0df822271cc20199d
lovenery <edittye379@gmail.com>
2016-05-30 22:00:04 +0800

    file upload success!

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Auth;
+use Image;
 
 class UserController extends Controller
 {
@@ -15,6 +16,16 @@ class UserController extends Controller
 
     public function update_avatar(Request $request){
         // Handle the user upload of avatar
+    	if($request->hasFile('avatar')){
+    		$avatar = $request->file('avatar');
+    		$filename = time() . '.' . $avatar->getClientOriginalExtension();
+    		Image::make($avatar)->resize(300, 300)->save( public_path('uploads/avatars/' . $filename ) );
 
+    		$user = Auth::user();
+    		$user->avatar = $filename;
+    		$user->save();
+    	}
+
+    	return view('profile', array('user' => Auth::user()) );
     }
 }
